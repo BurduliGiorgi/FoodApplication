6b0785e76a00c8142aa919c35e275bfced49b9c9
BurduliGiorgi <burdulig25@gmail.com>
2024-07-19 18:49:11 +0400

    PartOneDone

--- FoodApplication/obj/Debug/net8.0/FoodApplication.AssemblyInfo.cs
+++ FoodApplication/obj/Debug/net8.0/FoodApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20c5f4910311fdbd65dfcd64f2f074db5905fa64")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
