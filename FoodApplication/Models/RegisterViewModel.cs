1571f9b535f6b0663d9d726b20b7fcb7483bc53c
BurduliGiorgi <burdulig25@gmail.com>
2024-08-05 18:20:37 +0400

    Add Register Model

---
+++ FoodApplication/Models/RegisterViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FoodApplication.Models
+{
+    public class RegisterViewModel
+    {
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        public string? Address { get; set; }
+        [Required,EmailAddress]
+        public string? Email { get; set;}
+        [Required]
+        public string? Password { get; set; }
+
+    }
+}
