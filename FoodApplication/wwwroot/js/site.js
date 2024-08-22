07e91d2e81efa879a00ab4cc051b5b12fd9ea0a7
BurduliGiorgi <burdulig25@gmail.com>
2024-08-22 22:57:30 +0400

    Quantity Increse & Decrese

--- FoodApplication/wwwroot/js/site.js
+++ FoodApplication/wwwroot/js/site.js
@@ -48,3 +48,17 @@ function showOrderRecipeDetails(orderRecipeDetails, showId) {
         }
     });
 }
+
+//order page
+
+
+function quantity(option) {
+    let qty = $('#qty').val();
+    if (option === 'inc') {
+        qty = parseInt(qty)+1;
+    }
+    else {
+        qty = qty == 1 ? qty : qty - 1;
+    }
+    $('#qty').val(qty);
+}
