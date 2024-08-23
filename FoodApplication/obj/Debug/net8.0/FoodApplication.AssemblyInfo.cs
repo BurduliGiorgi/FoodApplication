a529690629f9c4340d29d281e9426b72391e8d79
BurduliGiorgi <burdulig25@gmail.com>
2024-08-23 18:26:04 +0400

    Add Cart Icon In Menu

--- FoodApplication/obj/Debug/net8.0/FoodApplication.AssemblyInfo.cs
+++ FoodApplication/obj/Debug/net8.0/FoodApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a319295acab85f7245511213b58bcdfa0c3ea5da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7781142fe76df32d741ddac1e9e9377f3ebdd5a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FoodApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
