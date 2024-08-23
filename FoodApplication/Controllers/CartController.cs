79f32ff813f37db0b4982263779e313c30dc3bb1
BurduliGiorgi <burdulig25@gmail.com>
2024-08-23 18:30:29 +0400

    Add Cart Contrller and View

---
+++ FoodApplication/Controllers/CartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FoodApplication.Controllers
+{
+    public class CartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
